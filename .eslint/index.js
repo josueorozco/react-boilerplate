40ebafc562d43e73add35390608ac42fab0c6da9
Josue Orozco <jorozco@billpaysolutions.com>
2016-11-10 20:37:50 -0800

    Cleaning up deprecated code. Moving webpack import/resolver settings to .eslintrc.js in root

--- .eslint/index.js
+++ .eslint/index.js
@@ -53,99 +53,11 @@ module.exports = {
         'import/prefer-default-export': 0,
         'import/no-extraneous-dependencies': 0,
         'import/no-named-as-default': 0,
+        'import/no-webpack-loader-syntax': 0,
+        'import/no-dynamic-require': 0,
+        'react/require-extension': 0,
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         'react/jsx-filename-extension': 0
-    },
-    settings: {
-        'import/resolver': {
-            webpack: {
-                config: './internals/webpack/webpack.test.babel.js'
-            }
-        }
     }
 };
-
-// 'env': {
-//   'browser': true,
-//   'node': true,
-//   'mocha': true,
-//   'es6': true
-// },
-// 'plugins': [
-//   'react',
-//   'jsx-a11y'
-// ],
-// 'parserOptions': {
-//   'ecmaVersion': 6,
-//   'sourceType': 'module',
-//   'ecmaFeatures': {
-//     'jsx': true
-//   }
-// },
-// 'rules': {
-//   'arrow-body-style': [
-//     2,
-//     'as-needed'
-//   ],
-//   'comma-dangle': [
-//     2,
-//     'never'
-//   ],
-//   'import/imports-first': 1,
-//   'import/newline-after-import': 0,
-//   'import/no-extraneous-dependencies': 0,
-//   'import/no-named-as-default': 0,
-//   'import/no-unresolved': 2,
-//   'import/prefer-default-export': 0,
-//   'indent': [
-//     2,
-//     4,
-//     {
-//       'SwitchCase': 1,
-//       'VariableDeclarator': 1,
-//       'outerIIFEBody': 0,
-//       'MemberExpression': 1,
-//       'FunctionDeclaration': {
-//         'body': 1,
-//         'parameters': 2
-//       },
-//       'FunctionExpression': {
-//         'body': 1,
-//         'parameters': 2
-//       }
-//     }
-//   ],
-//   'jsx-a11y/aria-props': 2,
-//   'jsx-a11y/heading-has-content': 0,
-//   'jsx-a11y/href-no-hash': 2,
-//   'jsx-a11y/label-has-for': 2,
-//   'jsx-a11y/mouse-events-have-key-events': 2,
-//   'jsx-a11y/role-has-required-aria-props': 2,
-//   'jsx-a11y/role-supports-aria-props': 2,
-//   'max-len': 0,
-//   'newline-per-chained-call': 0,
-//   'no-console': 1,
-//   'no-use-before-define': 1,
-//   'prefer-template': 2,
-//   'react/jsx-indent': [
-//     2,
-//     4
-//   ],
-//   'react/jsx-indent-props': [
-//     2,
-//     4
-//   ],
-//   'react/jsx-filename-extension': 0,
-//   'react/jsx-no-target-blank': 0,
-//   'react/require-extension': 0,
-//   'react/self-closing-comp': 0,
-//   'require-yield': 0
-// },
-// 'settings': {
-//   'import/resolver': {
-//     'webpack': {
-//       'config': './internals/webpack/webpack.test.babel.js'
-//     }
-//   }
-// }
--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,17 @@
+const path = require('path');
+
 module.exports = {
     root: true,
     parser: 'babel-eslint',
     extends: [
         'airbnb',
         './.eslint/index.js'
-    ]
+    ],
+    settings: {
+        'import/resolver': {
+            webpack: {
+                config: path.resolve(process.cwd(), 'internals/webpack/webpack.dev.babel')
+            }
+        }
+    }
 };
