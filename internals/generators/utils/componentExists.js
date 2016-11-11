850933bf719a388a6db1c538bc74c990a22994ce
Josue Orozco <jorozco@surgeforward.com>
2016-11-10 16:33:26 -0800

    Correcting linting errors in internals/generators

--- internals/generators/utils/componentExists.js
+++ internals/generators/utils/componentExists.js
@@ -5,12 +5,13 @@
  */
 
 const fs = require('fs');
+
 const pageComponents = fs.readdirSync('app/components');
 const pageContainers = fs.readdirSync('app/containers');
 const components = pageComponents.concat(pageContainers);
 
 function componentExists(comp) {
-  return components.indexOf(comp) >= 0;
+    return components.indexOf(comp) >= 0;
 }
 
 module.exports = componentExists;
